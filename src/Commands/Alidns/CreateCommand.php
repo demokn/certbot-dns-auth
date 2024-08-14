91436f4a7542b1f25d7586b7e789d6526838549d
demo_knyang <demo.knyang@gmail.com>
2024-08-14 22:33:56 +0800

    Require php>=8.1

--- src/Commands/Alidns/CreateCommand.php
+++ src/Commands/Alidns/CreateCommand.php
@@ -3,15 +3,15 @@
 namespace Demokn\DnsAuth\Commands\Alidns;
 
 use Demokn\DnsAuth\Facades\Alidns;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'alidns:create')]
 class CreateCommand extends Command
 {
-    protected static $defaultName = 'alidns:create';
-
     protected function configure()
     {
         $this->setDescription('修改 Aliyun DNS 解析记录')
@@ -25,7 +25,7 @@ class CreateCommand extends Command
             ->addArgument('app_secret', InputArgument::REQUIRED, '阿里云 AccessKey Secret');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
         $alidns = Alidns::createClient($input->getArgument('app_key'), $input->getArgument('app_secret'));
 
@@ -44,5 +44,7 @@ class CreateCommand extends Command
         }
 
         $output->writeln(json_encode($result->toArray()));
+
+        return Command::SUCCESS;
     }
 }
--- src/Commands/Alidns/DeleteCommand.php
+++ src/Commands/Alidns/DeleteCommand.php
@@ -3,15 +3,15 @@
 namespace Demokn\DnsAuth\Commands\Alidns;
 
 use Demokn\DnsAuth\Facades\Alidns;
+use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
+#[AsCommand(name: 'alidns:delete')]
 class DeleteCommand extends Command
 {
-    protected static $defaultName = 'alidns:delete';
-
     protected function configure()
     {
         $this->setDescription('删除 Aliyun DNS 解析记录')
@@ -23,7 +23,7 @@ class DeleteCommand extends Command
             ->addArgument('app_secret', InputArgument::REQUIRED, '阿里云 AccessKey Secret');
     }
 
-    public function execute(InputInterface $input, OutputInterface $output)
+    public function execute(InputInterface $input, OutputInterface $output): int
     {
         $alidns = Alidns::createClient($input->getArgument('app_key'), $input->getArgument('app_secret'));
 
@@ -55,5 +55,7 @@ class DeleteCommand extends Command
         if ($deleteResult->getStatusCode() >= 400) {
             throw new \RuntimeException('Request aliyun failed.');
         }
+
+        return Command::SUCCESS;
     }
 }
